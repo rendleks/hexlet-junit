0cb6ba9d654c1a02c81fa3aaabb89c0ab6235a1c
rendleks <0rendl050@gmail.com>
2026-03-15 11:42:02 +0300

    add Test

---
+++ src/test/java/hexlet/code/SomeClassTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import hexlet.code.SomeClass;
+import org.junit.jupiter.api.Test;
+
+class SomeClassTest {
+
+    @Test
+    public void testSum() {
+        var expected = 5;
+        var actual = SomeClass.sum(3, 2);
+        assertEquals(expected, actual);
+    }
+}
